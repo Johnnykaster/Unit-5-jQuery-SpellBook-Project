d95861e040e74c4d0873724c3486f2dfbfa34276
JohnnyKaster <44278869+Johnnykaster@users.noreply.github.com>
2019-01-31 16:46:47 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+//Spell #1
+
+$(".button1").click(function(){
+    $(".start").toggle();
+    $(".firstspell").toggle();
+});
+
+
+
+//Spell #2
+
+$(".spell").click(function(){
+    $(".beetles").toggle();
+});
+
+
+
+//Spell #3
+
+$(".onetime").click(function(){
+    $(".onetime").hide();
+});
+
+$(".shreck").click(function(){
+    $(".hello").toggle();
+});
+
+
+
